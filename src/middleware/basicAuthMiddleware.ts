0261c3373b6e710f17791d133b6fc67bccc51304
Artur <82472015+benyaar@users.noreply.github.com>
2022-11-10 15:57:00 +0200

    HW 2 update validator code

--- src/middleware/basicAuthMiddleware.ts
+++ src/middleware/basicAuthMiddleware.ts
@@ -1,7 +1,7 @@
 import {NextFunction, Request, Response} from "express";
 
 export const basicAuthMiddleware = (req:Request, res: Response, next:NextFunction) => {
-    if(!req.headers.authorization) return res.sendStatus(400)
+    if(!req.headers.authorization) return res.sendStatus(401)
     const authBasicPassword = req.headers.authorization.split(' ')[1]
     if(authBasicPassword === 'YWRtaW46cXdlcnR5'){
         next()
