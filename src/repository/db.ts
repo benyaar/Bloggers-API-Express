df6dcafa46a930db7b62db4f66b75d0d16403aa1
Artur <82472015+benyaar@users.noreply.github.com>
2022-11-12 15:01:15 +0200

    HW 4 add change mongoUri

--- src/repository/db.ts
+++ src/repository/db.ts
@@ -2,9 +2,9 @@ import {MongoClient} from "mongodb"
 import {BlogDBType, PostDBType, VideoDBType} from "../types/types";
 
 
-//"mongodb+srv://admin:admin@cluster0.9zvor.mongodb.net/?retryWrites=true&w=majority" ||
+//"mongodb+srv://admin:admin@cluster0.9zvor.mongodb.net/?retryWrites=true&w=majority"
 
-const mongoURi = process.env.mongoURI || "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.1"
+const mongoURi = process.env.mongoURI || "mongodb+srv://admin:admin@cluster0.9zvor.mongodb.net/?retryWrites=true&w=majority"
 
 export const client = new MongoClient(mongoURi)
 export async function runDB() {
