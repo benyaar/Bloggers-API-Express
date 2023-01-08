8fab095563ea7ee594cb9eeac2f38a87dce10516
Artur <82472015+benyaar@users.noreply.github.com>
2023-01-08 22:21:35 +0200

    add cors in vercel

--- src/index.ts
+++ src/index.ts
@@ -16,12 +16,12 @@ import {userSessionsRouter} from "./router/userSessionsRouter";
 const app = express()
 const port = process.env.PORT || 3000
 
-// const corsOptions ={
-//     origin:'http://localhost:3000',
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
+const corsOptions ={
+    origin:'http://localhost:3000',
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(bodyParser.json())
 
 app.use(cookieParser())
