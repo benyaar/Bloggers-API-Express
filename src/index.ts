44e34d2b60fb0f98cf6b8b1dea1ff1d537f2be5f
Artur <82472015+benyaar@users.noreply.github.com>
2023-01-09 20:45:18 +0200

    add /api

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,6 @@ import {commentsRouter} from "./router/commentsRouter";
 import cookieParser from "cookie-parser";
 import {userSessionsRouter} from "./router/userSessionsRouter";
 
-
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -21,18 +20,18 @@ const corsOptions ={
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
-
+app.use(cors(corsOptions));
 app.use(bodyParser.json())
-app.use(cors(corsOptions))
+
 app.use(cookieParser())
-app.use("/videos", videosRouter)
-app.use("/testing", deleteDataRouter)
-app.use("/blogs", bloggersRouter)
-app.use("/posts", postsRouter)
-app.use("/users", usersRouter)
-app.use("/auth", authRouter)
-app.use("/comments", commentsRouter)
-app.use("/security", userSessionsRouter)
+app.use("/api/videos", videosRouter)
+app.use("/api/testing", deleteDataRouter)
+app.use("/api/blogs", bloggersRouter)
+app.use("/api/posts", postsRouter)
+app.use("/api/users", usersRouter)
+app.use("/api/auth", authRouter)
+app.use("/api/comments", commentsRouter)
+app.use("/api/security", userSessionsRouter)
 app.set('trust proxy', true)
 
 const startApp = async ()=>{
